3eb6842a4cd1b2628369061f7eeeb4cac80a0cbf
sLeeNguyen <nguyenngoc1311999@gmail.com>
2020-05-25 08:28:03 +0700

    create Resource class

---
+++ space_war/src/helpers/Resource.java
@@ -0,0 +1,12 @@
+package helpers;
+
+public class Resource {
+	public static final String[][] ENEMY_PATH = {
+			{"/resources/enemy/enemyBlack1.png", "/resources/enemy/enemyBlack2.png", "/resources/enemy/enemyBlack3.png", "/resources/enemy/enemyBlack4.png", "/resources/enemy/enemyBlack5.png"},
+			{"/resources/enemy/enemyBlue1.png", "/resources/enemy/enemyBlue2.png", "/resources/enemy/enemyBlue3.png", "/resources/enemy/enemyBlue4.png", "/resources/enemy/enemyBlue5.png"},
+			{"/resources/enemy/enemyGreen1.png", "/resources/enemy/enemyGreen2.png", "/resources/enemy/enemyGreen3.png", "/resources/enemy/enemyGreen4.png", "/resources/enemy/enemyGreen5.png"},
+			{"/resources/enemy/enemyRed1.png", "/resources/enemy/enemyRed2.png", "/resources/enemy/enemyRed3.png", "/resources/enemy/enemyRed4.png", "/resources/enemy/enemyRed5.png"}}; 
+	public static final int ENEMY_HEIGHT = 84;
+	public static final int ENEMY_WIDTH = 100;
+	public static final int ENEMY_RADIUS = 35;
+}
